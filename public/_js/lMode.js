2c01db1022146310b8c42a2d066457ef1f2d4669
Sisanda <162667618+anda031015@users.noreply.github.com>
2025-01-02 23:45:10 +0200

    Update lMode.js
    Manual deployment from neocities

--- public/_js/lMode.js
+++ public/_js/lMode.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
   var themeButton = document.getElementById('themeButton');
   var body = document.body;
-
+  
   themeButton.addEventListener('click', function() {
     if (body.classList.contains('light-theme')) {
       body.classList.remove('light-theme');
-      themeButton.textContent = 'Light Theme';
+      themeButton.textContent = '';
     } else {
       body.classList.add('light-theme');
-      themeButton.textContent = 'Dark Theme';
+      themeButton.textContent = '';
     }
   });
 });
